a3a3251b50ec0396a28aa4cdf6d32c2eb56f8408
Satish Patel <tosatishpatel@gmail.com>
2018-04-13 18:10:22 +0530

    FMO: do not use gplay related variables
    Signed-off-by: Satish Patel <tosatishpatel@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -1,14 +1,15 @@
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
-ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.com.google.clientidbase=android-google
-else
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.com.google.clientidbase=$(PRODUCT_GMS_CLIENTID_BASE)
-endif
+# FMO - Do not intend to use gms
+#ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
+#PRODUCT_PROPERTY_OVERRIDES += \
+#    ro.com.google.clientidbase=android-google
+#else
+#PRODUCT_PROPERTY_OVERRIDES += \
+#    ro.com.google.clientidbase=$(PRODUCT_GMS_CLIENTID_BASE)
+#endif
 
-PRODUCT_PROPERTY_OVERRIDES += \
+#PRODUCT_PROPERTY_OVERRIDES += \
     keyguard.no_require_sim=true \
     dalvik.vm.debug.alloc=0 \
     ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
