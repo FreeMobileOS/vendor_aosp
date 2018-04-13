a0197038e10134dade2f3ee4daca6f584527701b
Satish Patel <tosatishpatel@gmail.com>
2018-04-13 09:10:52 +0530

    FMO: changed branding to FMO
    Do not use CUSTOM_BUILD, instead use PRODUCT_DEVICE
    
    Signed-off-by: Satish Patel <tosatishpatel@gmail.com>

--- config/branding.mk
+++ config/branding.mk
@@ -1,17 +1,18 @@
 # Set all versions
+CUSTOM_BUILD := $(PRODUCT_DEVICE)
 CUSTOM_BUILD_TYPE ?= UNOFFICIAL
 CUSTOM_BUILD_DATE := $(shell date -u +%Y%m%d-%H%M)
-CUSTOM_VERSION := PixelExperience_$(CUSTOM_BUILD)-$(PLATFORM_VERSION)-$(CUSTOM_BUILD_DATE)-$(CUSTOM_BUILD_TYPE)
+CUSTOM_VERSION := fmo_$(CUSTOM_BUILD)-$(PLATFORM_VERSION)-$(CUSTOM_BUILD_DATE)-$(CUSTOM_BUILD_TYPE)
 
 PRODUCT_PROPERTY_OVERRIDES += \
     BUILD_DISPLAY_ID=$(BUILD_ID) \
-    org.pixelexperience.version=$(CUSTOM_VERSION) \
-    org.pixelexperience.build_date=$(CUSTOM_BUILD_DATE) \
-    org.pixelexperience.build_type=$(CUSTOM_BUILD_TYPE)
+    org.fmo.version=$(CUSTOM_VERSION) \
+    org.fmo.build_date=$(CUSTOM_BUILD_DATE) \
+    org.fmo.build_type=$(CUSTOM_BUILD_TYPE)
 
 # Fingerprint
 TARGET_PRODUCT_SHORT := $(CUSTOM_BUILD)
 TARGET_PRODUCT_SHORT := $(subst aosp_,,$(TARGET_PRODUCT_SHORT))
-ROM_FINGERPRINT := PixelExperience/$(PLATFORM_VERSION)/$(TARGET_PRODUCT_SHORT)/$(shell date +%Y%m%d.%H:%M)
+ROM_FINGERPRINT := fmo/$(PLATFORM_VERSION)/$(TARGET_PRODUCT_SHORT)/$(shell date +%Y%m%d.%H:%M)
 PRODUCT_PROPERTY_OVERRIDES += \
-    org.pixelexperience.fingerprint=$(ROM_FINGERPRINT)
+    org.fmo.fingerprint=$(ROM_FINGERPRINT)
